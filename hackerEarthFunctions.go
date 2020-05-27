521d63f098e8c8b944834f3436fc322dce794a94
ani1311 <aniket.alur13111997@gmail.com>
2020-05-27 23:29:27 +0530

    refactor(utils): made getNode generic

--- hackerEarthFunctions.go
+++ hackerEarthFunctions.go
@@ -8,7 +8,7 @@ import (
 func getChllanges() []Challenge {
 	htm, _ := html.Parse(getPage("https://www.hackerearth.com/challenges/"))
 	challenges := make([]Challenge, 0)
-	challengesNode := getChallengeNode(htm)
+	challengesNode := getChallengeNode(htm, atom.Div, "class", "ongoing challenge-list")
 	t := challengesNode.FirstChild
 	for t != nil {
 		if len(t.Attr) != 0 && t.Attr[0].Key == "class" && t.Attr[0].Val == "challenge-card-modern" {
@@ -21,28 +21,6 @@ func getChllanges() []Challenge {
 	// }
 
 	return challenges
-}
-
-func getChallengeNode(n *html.Node) *html.Node {
-	if n == nil {
-		return nil
-	}
-	t := n.FirstChild
-	for t != nil {
-		if t.DataAtom == atom.Div {
-			for j := 0; j < len(t.Attr); j++ {
-				if t.Attr[j].Key == "class" && t.Attr[j].Val == "ongoing challenge-list" {
-					return t
-				}
-			}
-		}
-		chalNode := getChallengeNode(t)
-		if chalNode != nil {
-			return chalNode
-		}
-		t = t.NextSibling
-	}
-	return nil
 }
 
 func getChallenge(n *html.Node) Challenge {
--- utils.go
+++ utils.go
@@ -5,6 +5,9 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
 )
 
 func getPage(page string) io.ReadCloser {
@@ -15,4 +18,48 @@ func getPage(page string) io.ReadCloser {
 func getPageFromLocal(page string) io.ReadCloser {
 	dat, _ := ioutil.ReadFile(page)
 	return ioutil.NopCloser(bytes.NewReader(dat))
+}
+
+func getChallengeNode(n *html.Node, atomType atom.Atom, attribKey string, attribVal string) *html.Node {
+	if n == nil {
+		return nil
+	}
+	t := n.FirstChild
+	for t != nil {
+		if t.DataAtom == atomType {
+			for j := 0; j < len(t.Attr); j++ {
+				if t.Attr[j].Key == attribKey && t.Attr[j].Val == attribVal {
+					return t
+				}
+			}
+		}
+		chalNode := getChallengeNode(t, atomType, attribKey, attribVal)
+		if chalNode != nil {
+			return chalNode
+		}
+		t = t.NextSibling
+	}
+	return nil
+}
+
+func getChallengeNode(n *html.Node, atomType atom.Atom, tagVal string) *html.Node {
+	if n == nil {
+		return nil
+	}
+	t := n.FirstChild
+	for t != nil {
+		if t.DataAtom == atomType {
+			for j := 0; j < len(t.Attr); j++ {
+				if t.Attr[j].Key == attribKey && t.Attr[j].Val == attribVal {
+					return t
+				}
+			}
+		}
+		chalNode := getChallengeNode(t, atomType, tagVal)
+		if chalNode != nil {
+			return chalNode
+		}
+		t = t.NextSibling
+	}
+	return nil
 }
